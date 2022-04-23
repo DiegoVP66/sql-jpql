108a5dc2b39351c0fb26ea16e86444b65a0734d7
DiegoVP66 <dvpdev33@gmail.com>
2022-04-23 20:04:44 -0300

    Uri2608 created

---
+++ uri2608/src/test/java/com/diego/uri2608/Uri2608ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.uri2608;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri2608ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
