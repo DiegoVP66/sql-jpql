5f978ee3bbf953e6bd238dd5a7c385f27f700c86
DiegoVP66 <dvpdev33@gmail.com>
2022-04-23 19:46:54 -0300

    Uri2607 SQL

--- uri2607/src/main/java/com/diego/uri2607/Uri2607Application.java
+++ uri2607/src/main/java/com/diego/uri2607/Uri2607Application.java
@@ -1,13 +1,38 @@
 package com.diego.uri2607;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.diego.uri2607.dto.ProviderMinDTO;
+import com.diego.uri2607.projections.ProviderMinProjection;
+import com.diego.uri2607.repositories.ProviderRepository;
+
 @SpringBootApplication
-public class Uri2607Application {
+public class Uri2607Application implements CommandLineRunner {
+
+	@Autowired
+	private ProviderRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2607Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<ProviderMinProjection> list = repository.search1();
+		List<ProviderMinDTO> result1 = list.stream().map(x -> new ProviderMinDTO(x)).collect(Collectors.toList());
+
+		System.out.println("\n***SQL RAIZ");
+		for (ProviderMinDTO obj : result1) {
+			System.out.println(obj);
+		}
+		System.out.println("\n\n\n");
+
 	}
 
 }
--- uri2607/src/main/java/com/diego/uri2607/repositories/ProviderRepository.java
+++ uri2607/src/main/java/com/diego/uri2607/repositories/ProviderRepository.java
@@ -1,11 +1,19 @@
 package com.diego.uri2607.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.diego.uri2607.entities.Provider;
+import com.diego.uri2607.projections.ProviderMinProjection;
 
 @Repository
 public interface ProviderRepository extends JpaRepository<Provider, Long> {
 
+	@Query(nativeQuery = true, value = "SELECT DISTINCT providers.city " 
+			+ "FROM providers "
+			+ "ORDER BY providers.city ASC")
+	List<ProviderMinProjection> search1();
 }
