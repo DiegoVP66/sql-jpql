409ac597f13d3cc6aae5f869197c561324fed1f6
DiegoVP66 <dvpdev33@gmail.com>
2022-04-25 21:23:12 -0300

    Uri2605 created

---
+++ uri2605/src/test/java/com/diego/uri2605/Uri2605ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.uri2605;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri2605ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
